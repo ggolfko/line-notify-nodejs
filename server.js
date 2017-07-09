76b43a2159fd646cbed4eed1123075c9ad063c4d
Kitti Onthong <5535512134@email.psu.ac.th>
2017-07-10 02:29:11 +0700

    Add web scanning using cheerio

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 const request = require('request')
 const axios = require('axios');
 const Monitor = require('monitor');
+const Hapi = require('hapi');
+const cheerio = require('cheerio');
 
-/**
- * baseURL json
- * @type {[type]}
- */
 const instance = axios.create({
   baseURL: 'https://api.nanopool.org/v1/zec/balance_hashrate'
 });
 
-/**
- * options for processMonitor
- * @type {Object}
- */
+const server = new Hapi.Server();
+server.connection({
+  host: 'localhost',
+  port: 8080
+});
+
+
 let options = {
   probeClass: 'Process',
   initParams: {
@@ -22,17 +23,24 @@ let options = {
 }
 
 const processMonitor = new Monitor(options);
-
-/**
- * line token
- * get token https://notify-bot.line.me/
- */
 const token = 'lineToken';
 
-/**
- * [loadData description]
- * @return {[type]} [description]
- */
+getData = () => {
+  return new Promise((resolve, reject) => {
+    let url = `http://burstcoin.biz/address/11838271257372488268`;
+    request(url, (err, res, body) => {
+      if (!err && res.statusCode === 200) {
+        let $ = cheerio.load(body);
+        let balance = $('abbr').eq(0).text().trim();
+        console.log('loaded ' + balance);
+        resolve(balance);
+      } else {
+        reject("if error in loaded");
+      }
+    });
+  })
+}
+
 loadData = () => {
   return new Promise((resolve, reject) => {
     instance.get('/t1WXMkKKQnkF9GvDyhrGRzKD69YQac9QSjJ').then((res) => {
@@ -43,16 +51,11 @@ loadData = () => {
   })
 }
 
-/**
- * processMonitor on change
- * @type {String}
- */
+
 processMonitor.on('change', () => {
   loadData().then((res) => {
-
     //console.log(JSON.stringify(res));
     let message = 'status: ' + res.status + ' hashrate: ' + res.data.hashrate + ' balance: ' + res.data.balance;
-
     request({
       method: 'POST',
       uri: 'https://notify-api.line.me/api/notify',
@@ -76,13 +79,49 @@ processMonitor.on('change', () => {
 
 });
 
-/**
- * processMonitor when error
- * @type {String}
- */
 processMonitor.connect((err) => {
   if (err) {
     console.error('Error connecting with the process probe: ', err);
     process.exit(1);
   }
 });
+
+// server.route({
+//   method: 'GET',
+//   path: '/',
+//
+//   handler: (req, reply) => {
+//
+//     let appId = req.params.appId;
+//
+//     let url = `http://burstcoin.biz/address/11838271257372488268`;
+//
+//     request(url, (err, res, body) => {
+//
+//       if (!err && res.statusCode === 200) {
+//
+//         let $ = cheerio.load(body);
+//
+//         let balance = $('abbr').eq(0).text().trim();
+//
+//         console.log(balance);
+//
+//         reply({
+//           data: {
+//             balance: balance
+//           }
+//         });
+//
+//       } else {
+//         reply({
+//           message: `We're sorry, the requested ${url} was not found on this server.`
+//         });
+//       }
+//     });
+//
+//   }
+// });
+//
+// server.start(err => {
+//   console.log(`Server running at ${server.info.uri}`);
+// });
